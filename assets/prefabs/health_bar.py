a6788186ca049b5e0e4bdd5e6b26629d68d91587
Louis Jarraud <diamondsbattle@tutanota.com>
2020-09-02 18:20:32 +0200

    Added health_bar.py

---
+++ assets/prefabs/health_bar.py
@@ -0,0 +1,7 @@
+from ursina import *
+from ursina.prefabs.health_bar import HealthBar as Bar
+
+class HealthBar(Bar):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        pass
--- assets/scripts/wait.py
+++
@@ -1,7 +0,0 @@
-from ursina import *
-from threading import Thread
-from time import sleep
-
-def wait(delay):
-    t = Thread(target=Func(sleep, delay))
-    t.start()

0b8c63afce545b875d08c5b8e9750135bb8872d1
Louis Jarraud <diamondsbattle@tutanota.com>
2020-09-05 18:25:03 +0200

    main.py update

--- main.py
+++ main.py
@@ -5,14 +5,25 @@ from keybinds import keybind
 
 
 def update():
-    global ammo_text, current_weapon_text
+    global ammo_text, current_weapon_text, pickup_text
 
-    ammo_text.text = f'{current_weapon.magazine}/{current_weapon.ammo}'
-    current_weapon_text.text = f'{current_weapon.name} ({current_weapon.mode})'
+    try:
+        ammo_text.text = f'{current_weapon.magazine}/{current_weapon.ammo}'
+        current_weapon_text.text = f'{current_weapon.name} ({current_weapon.mode})'
+        if current_weapon.parent != controller:
+            current_weapon.parent = controller
+    except Exception:
+        ammo_text.text = ''
+        current_weapon_text.text = ''
+
 
 def input(key):
-    if key == keybind['gun_change_mode']:
+    global current_weapon
+    if key == keybind['gun_change_mode'] and current_weapon:
         current_weapon.mode = 'semi' if current_weapon.mode == 'auto' else 'auto'
+    if key == keybind['drop'] and current_weapon:
+        current_weapon.parent = scene
+        current_weapon = None
 
 
 if __name__ == '__main__':
@@ -34,13 +45,24 @@ if __name__ == '__main__':
                   magazine=10,
                   reload_delay=1,
                   name='M9',
-                  model='gun_pistol',
-                  parent=controller,
+                  model='cube',
                   texture='grass',
                   collider='cube',
                   shoot_delay=.001,
                   mode='semi',
                   position=Vec3(0, 0, 0))
+
+    m4 = Weapon(ammo=360,
+                mag_size=30,
+                magazine=30,
+                reload_delay=3,
+                name='M4',
+                model='cube',
+                color=color.pink,
+                collider='cube',
+                shoot_delay=.001,
+                mode='semi',
+                position=Vec3(0, 0, 0))
 
     current_weapon = g18c
 
@@ -49,5 +71,7 @@ if __name__ == '__main__':
                      position=Vec3(.52, -.4, 0))
     current_weapon_text = Text(text=f'{current_weapon.name} ({current_weapon.mode})',
                                position=Vec3(.53, -.3, 0))
+    pickup_text = Text(text='',
+                       position=Vec3(.54, -.2, 0))
 
     app.run()

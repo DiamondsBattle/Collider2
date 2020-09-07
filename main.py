457645622e15bbed49c40a7938b1193379e18781
Louis Jarraud <diamondsbattle@tutanota.com>
2020-09-07 18:49:16 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from ursina import *
 from assets.prefabs.controller import FirstPersonController
 from assets.prefabs.weapon import Weapon
+from assets.prefabs.map import Map
 from keybinds import keybind
+from ursina.shaders.basic_lighting import basic_lighting_shader
 
 
 def update():
@@ -40,31 +42,41 @@ if __name__ == '__main__':
                     texture_scale=(32, 32),
                     collider='box',
                     filtering=None)
-    g18c = Weapon(ammo=54,
-                  mag_size=10,
-                  magazine=10,
-                  reload_delay=1,
-                  name='M9',
-                  model='cube',
-                  texture='grass',
-                  collider='cube',
-                  shoot_delay=.001,
-                  mode='semi',
-                  position=Vec3(0, 0, 0))
-
-    m4 = Weapon(ammo=360,
-                mag_size=30,
-                magazine=30,
-                reload_delay=3,
-                name='M4',
-                model='cube',
-                color=color.pink,
-                collider='cube',
-                shoot_delay=.001,
+    m9 = Weapon(ammo=54,
+                mag_size=15,
+                magazine=15,
+                reload_delay=2,
+                name='M9',
+                model='m9',
+                collider='box',
+                shoot_delay=.2,
                 mode='semi',
-                position=Vec3(0, 0, 0))
+                position=Vec3(0, 0, 0),
+                scale=.2,
+                shader=basic_lighting_shader)
+    scar_l = Weapon(ammo=360,
+                    mag_size=30,
+                    magazine=30,
+                    reload_delay=3,
+                    name='Scar-L',
+                    model='scar_l',
+                    collider='box',
+                    shoot_delay=.08,
+                    mode='auto',
+                    position=Vec3(0, 0, 0),
+                    scale=.01,
+                    shader=basic_lighting_shader)
 
-    current_weapon = g18c
+    map = Map(name='bureau',
+              # model='building',
+              shader=basic_lighting_shader,
+              collider='none',
+              y=6)
+
+    current_weapon = scar_l
+    current_weapon.parent = controller
+    current_weapon.position = Vec3(.8, 1.2, 1.5)
+    current_weapon.rotation = Vec3(0, -180, 0) if current_weapon == scar_l else current_weapon.rotation
 
     ammo_text = Text(text=f'{current_weapon.magazine}/{current_weapon.ammo}',
                      scale=3,
